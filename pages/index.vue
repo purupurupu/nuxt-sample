c740f5ddfd5393d587fea1f189261baabbbf71a5
purupurupu <purupurupu0prog@gmail.com>
2024-06-03 22:28:27 +0900

    feat:add page routing

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to the home page!</p>
+  </div>
+  ;
+</template>
+<script setup lang="ts">
+// export default {};
+</script>
+<style scoped>
+h2 {
+  color: blue;
+}
+p {
+  color: red;
+}
+</style>
