2730658833ac6af34ef6355aa2d27a1d19094fdd
purupurupu <purupurupu0prog@gmail.com>
2024-06-04 23:35:44 +0900

    fix:header link

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">
+              Home
+            </NuxtLink>
+            <NuxtLink to="/events">
+              Events
+            </NuxtLink>
+          </li>
+        </ul>
+      </header>
       <NuxtPage />
     </NuxtLayout>
   </div>
