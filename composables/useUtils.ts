cf628d3b3464431b40d7dbbcb013b9bb7fdba1b3
purupurupu <purupurupu0prog@gmail.com>
2024-06-09 21:14:21 +0900

    feat:add composables

---
+++ composables/useUtils.ts
@@ -0,0 +1,10 @@
+export const userUtils = () => {
+    const sayHello = () => {
+        console.log('Hello World');
+    }
+    const sayGoodbye = () => {
+        console.log('Goodbye World');
+    }
+
+    return {sayHello,sayGoodbye}
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <!-- if cancel theme default -->
-<!-- <script setup lang="ts">
-definePageMeta({
-  layout: "false",
-});
-</script> -->
+<script setup lang="ts">
+// definePageMeta({
+//   layout: "false",
+// });
+const {sayGoodbye} = userUtils()
+sayGoodbye()
+</script>
 
 <template>
   <div>
