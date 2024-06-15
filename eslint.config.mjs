23a9626dc01bb3379e2ac4dcd7216f52824d0a2d
purupurupu <purupurupu0prog@gmail.com>
2024-06-15 21:01:30 +0900

    fix:Fix incorrect ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,19 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   {
-    files: ["**/*.vue"],
-    rules: {
-      "vue/require-v-for-key": "error",
-      "vue/no-use-v-if-with-v-for": "error",
-    },
-  },
-  {
     files: ["**/*.vue", "**/*.ts"],
     rules: {
       // "no-console": "error",
       "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    rules: {
+      "vue/no-multiple-template-root": "error",
+      // "vue/multi-word-component-names": "error",
+      "vue/require-v-for-key": "error",
+      "vue/no-use-v-if-with-v-for": "error",
     },
   },
   stylistic.configs.customize({
