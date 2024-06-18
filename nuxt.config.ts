d6e656f2f560005f773ccf74b5747311ec09e88b
purupurupu <purupurupu0prog@gmail.com>
2024-06-18 23:01:21 +0900

    fix:meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Hello, Nuxt!",
+      meta: [{ name: "description", content: "this is a meta description" }],
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
---
+++ pages/meta.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "My App",
+  meta: [
+    { name: "description", content: "My amazing site." },
+  ],
+  bodyAttrs: {
+    class: "test",
+  },
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+</script>
