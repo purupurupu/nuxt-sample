b432e10a351c99e28e9dad6cf23df25d800203a0
purupurupu <purupurupu0prog@gmail.com>
2024-06-22 00:09:34 +0900

    fix:wip

---
+++ components/todos/TodoItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Todo } from "@/types/todo.interface";
+
+const props = defineProps<{
+  todo: Todo;
+}>();
+</script>
+
+<template>
+  <div>
+    <li>
+      <input type="checkbox">
+      <span> {{ props.todo.title }}</span>
+    </li>
+  </div>
+</template>
--- components/todos/TodoList.vue
+++ components/todos/TodoList.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
+import TodoItem from "./TodoItem.vue";
+
 const todos = [
-  { id: 1, text: "Learn Vue 3" },
-  { id: 2, text: "Learn TypeScript" },
-  { id: 3, text: "Learn Vite" },
+  { id: 1, title: "Learn Vue 3", completed: false },
+  { id: 2, title: "Learn TypeScript", completed: false },
+  { id: 3, title: "Learn Vite", completed: false },
 ];
 </script>
 
 <template>
   <div>
     <ul>
-      <li
+      <TodoItem
         v-for="todo in todos"
         :key="todo.id"
-      >
-        {{ todo.text }}
-      </li>
+        :todo="todo"
+      />
     </ul>
   </div>
 </template>
